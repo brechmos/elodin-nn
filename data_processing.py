66ef649ec56175cdef2116b7a38edfa63123bf00
Craig Jones <crjones@stsci.edu>
2018-02-14 14:38:13 -0500

    added in loader from json representation

--- data_processing.py
+++ data_processing.py
@@ -8,6 +8,23 @@ log = logging.getLogger("DataProcessing")
 log.setLevel(logging.INFO)
 
 
+class DataProcessingClass(object):
+    frametype_class_dict = {}
+
+    def __init__(self, *args, **kwargs):
+        self.frame_id = str(args[0].__name__)
+        DataProcessingClass.frametype_class_dict[self.frame_id] = args[0]
+
+    def __call__(self, cls):
+        if self.frame_id:
+            DataProcessingClass.frametype_class_dict[self.frame_id] = cls
+        return cls
+
+    @staticmethod
+    def get_class_from_frame_identifier(frame_identifier):
+        return DataProcessingClass.frametype_class_dict.get(frame_identifier)
+
+
 class DataProcessing:
     def __init__(self):
         pass
@@ -21,12 +38,22 @@ class DataProcessing:
                 output_data[:, :, ii] = func(input_data[:, :, ii], *args, **kwargs)
             return output_data
 
-    @abstractmethod:
     def save(self):
-        pass
+        raise NotImplementedError("Please Implement this method")
 
+    def process(self):
+        raise NotImplementedError("Please Implement this method")
+
+    @staticmethod
+    def load(parameters):
+        class_name = parameters.get('data_processing')
+        cls = DataProcessingClass.get_class_from_frame_identifier(class_name)
+        inst = cls()
+        inst.load(parameters)
+        return inst
+
+@DataProcessingClass
 class ZoomData(DataProcessing):
-
     def __init__(self, zoom_level=1):
         self._zoom_level = zoom_level
 
@@ -37,9 +64,12 @@ class ZoomData(DataProcessing):
         return self._apply2dfunc(input_data, scipy.ndimage.zoom, self._zoom_level)
 
     def save(self):
-        return {'data_processing': 'ZoomData', 'parameters': {'zoom': self._zoom_level}}
+        return {'data_processing': self.__class__.__name__, 'parameters': {'zoom': self._zoom_level}}
 
+    def load(self, parameters):
+        self._zoom_level = parameters.get('parameters').get('zoom')
 
+@DataProcessingClass
 class MedianFilterData(DataProcessing):
     def __init__(self, kernel_size=(1,1,1)):
         self._kernel_size = kernel_size
@@ -51,9 +81,13 @@ class MedianFilterData(DataProcessing):
         return scipy.ndimage.filters.median_filter(input_data, size=self._kernel_size)
 
     def save(self):
-        return {'data_processing': 'MedianFilterData', 'parameters': {'kernel_size': self._kernel_size}}
+        return {'data_processing': self.__class__.__name__, 'parameters': {'kernel_size': self._kernel_size}}
+
+    def load(self, parameters):
+        self._kernel_size = parameters.get('parameters').get('kernel_size')
 
 
+@DataProcessingClass
 class RotateData(DataProcessing):
     def __init__(self, angle=0.0):
         self._angle = angle
@@ -65,9 +99,13 @@ class RotateData(DataProcessing):
         return self._apply2dfunc(input_data, scipy.ndimage.rotate, self._angle, reshape=False)
 
     def save(self):
-        return {'data_processing': 'RotateData', 'parameters': {'angle': self._angle}}
+        return {'data_processing': self.__class__.__name__, 'parameters': {'angle': self._angle}}
+
+    def load(self, parameters):
+        self._angle = parameters.get('parameters').get('angle')
 
 
+@DataProcessingClass
 class GrayScaleData(DataProcessing):
     def __init__(self):
         pass
@@ -79,9 +117,13 @@ class GrayScaleData(DataProcessing):
         return np.dot(input_data[:,:,:3], [0.299, 0.587, 0.114])
 
     def save(self):
-        return {'data_processing': 'GrayScaleData', 'parameters': {}}
+        return {'data_processing': self.__class__.__name__, 'parameters': {}}
+
+    def load(self, parameters):
+        pass
 
 
+@DataProcessingClass
 class FlipUDData(DataProcessing):
     def __init__(self):
         pass
@@ -93,4 +135,7 @@ class FlipUDData(DataProcessing):
         return input_data[::-1]
 
     def save(self):
-        return {'data_processing': 'FlipUDData', 'parameters': {}}
+        return {'data_processing': self.__class__.__name__, 'parameters': {}}
+
+    def load(self, parameters):
+        pass
