ffc590f5efd0a4cd984b8583da2248bc49f6db24
Craig Jones <crjones@stsci.edu>
2018-02-25 11:38:14 -0500

    tweaked back

--- examples/runme_process.py
+++ examples/runme_process.py
@@ -1,25 +1,28 @@
 from similarity import tSNE
 from data_processing import MedianFilterData, ZoomData, RotateData, GrayScaleData
+from cutouts import BasicCutouts
 from fingerprint import FingerprintResnet, FingerprintInceptionV3
 from transfer_learning import TransferLearning, TransferLearningDisplay
 import glob
 
-input_file_pattern = '/Users/crjones/christmas/hubble/HSTHeritage/data/*.???'
+input_file_pattern = '/Users/crjones/christmas/hubble/HSTHeritage/data/a*.jpg'
 
-stepsize = 112
+step_size = 300
 
 input_filenames = glob.glob(input_file_pattern)
 
 fingerprint_model = FingerprintInceptionV3()
+
+basic_cutout = BasicCutouts(output_size=224, step_size=step_size)
 
 # # calculate fingerpirnts for median filtered
 
 #data_processing = [MedianFilterData((3, 3, 1)), GrayScaleData()]
 data_processing = []
 
-tl = TransferLearning(fingerprint_model, data_processing)
+tl = TransferLearning(basic_cutout, data_processing, fingerprint_model)
 tl.set_files(input_filenames)
 
-fingerprints = tl.calculate(stepsize=stepsize, display=True)
+fingerprints = tl.calculate(display=True)
 
 tl.save('hstheritage_fingerprints.pck')
