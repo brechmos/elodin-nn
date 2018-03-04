df1656b3e288b4c1fe3477270d11b4f9ab230dc0
Craig Jones <crjones@stsci.edu>
2018-03-04 11:52:49 -0500

    removed unneccessary imports

--- examples/runme_display.py
+++ examples/runme_display.py
@@ -1,21 +1,18 @@
 from similarity import tSNE, Jaccard, Distance
-from data_processing import MedianFilterData, ZoomData, RotateData, GrayScaleData
-from fingerprint import FingerprintResnet, FingerprintInceptionV3
 from transfer_learning import TransferLearning
 from transfer_learning_display import TransferLearningDisplay
 
-filename = 'acs.pck'
+filename = 'test.pck'
 
 tl = TransferLearning.load(filename)
 
 # Calculate hte similarity
 #similarity = Jaccard()
-#similarity = tSNE()
-similarity = Distance(metric='euclidean')
+similarity = tSNE(display_type='hexbin')
+#similarity = Distance(metric='cityblock')
 
 # Create the display with the calculated similarity
 tld = TransferLearningDisplay(similarity)
 
 # Display the figure
 tld.show(tl.fingerprints)
-
