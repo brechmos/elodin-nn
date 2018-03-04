aabdef6a178320dee12e32c96ae9852029caa0c8
Craig Jones <crjones@stsci.edu>
2018-03-04 11:51:26 -0500

    more commentsc

--- transfer_learning_display.py
+++ transfer_learning_display.py
@@ -21,6 +21,12 @@ class TransferLearningDisplay:
         self.sub_windows = None
 
     def show(self, fingerprints):
+        """
+        Overall display layout.
+
+        :param fingerprints:
+        :return:
+        """
         plt.show(block=False)
         plt.ion()
 
@@ -30,6 +36,7 @@ class TransferLearningDisplay:
         plt.gcf()
         self.axis = plt.axes([0.05, 0.05, 0.45, 0.45])
 
+        # Sub window for on move closest fingerprint
         self.axis_closest = plt.axes([0.5, 0.01, 0.2, 0.2])
         self.axis_closest.set_xticks([])
         self.axis_closest.set_yticks([])
@@ -41,8 +48,10 @@ class TransferLearningDisplay:
                                                     transform=self.axis_closest.transAxes,
                                                     color='w')
 
+        # Display the similarity plot (e.g., tSNE, jaccard etc)
         self.similarity.display(self.axis)
 
+        # Display the information text area
         self.info_axis = plt.axes([0.75, 0.11, 0.3, 0.05])
         self.info_axis.set_axis_off()
         self.info_axis.set_xticks([])
@@ -51,9 +60,7 @@ class TransferLearningDisplay:
         self.info_axis.set_ylabel('')
         self.info_text = self.info_axis.text(0, 0, 'Loading...', fontsize=8)
 
-        self._cid = self.fig.canvas.mpl_connect('button_press_event', self._onclick)
-        self.fig.canvas.mpl_connect('motion_notify_event', self._onmove)
-
+        # Display the 9 sub-windows
         self.sub_windows = []
         self.sub_data = []
         for row in range(3):
@@ -67,17 +74,34 @@ class TransferLearningDisplay:
                 self.sub_data.append(sd)
         plt.show(block=False)
 
+        # Attach the call backs
+        self._cid = self.fig.canvas.mpl_connect('button_press_event', self._onclick)
+        self.fig.canvas.mpl_connect('motion_notify_event', self._onmove)
+
+
     def _update_text(self, thetext):
+        """
+        Update the text in the information area
+
+        :param thetext: the text to display
+        :return:
+        """
         self.info_text.set_text(thetext)
         plt.draw()
 
     def _onmove(self, event):
+        """
+        What to do when the mouse moves
+
+        :param event: the event information
+        :return:
+        """
         log.debug('Moving to {}'.format(event))
+
+        # Do something if in the similarity axis
         if event.inaxes == self.axis:
             point = event.ydata, event.xdata
             close_fingerprint = self.similarity.find_similar(point, n=1)[0][1]
-
-            log.debug('Closest fingerprints {}'.format(close_fingerprint))
 
             row = close_fingerprint['row_min'], close_fingerprint['row_max']
             col = close_fingerprint['col_min'], close_fingerprint['col_max']
@@ -122,6 +146,7 @@ class TransferLearningDisplay:
                 self.sub_data[ii].set_data(np.zeros((224, 224)))
                 self.sub_windows[ii].redraw_in_frame()
 
+                # Display the data information for the fingerprint cutout
                 row = fingerprint['row_min'], fingerprint['row_max']
                 col = fingerprint['col_min'], fingerprint['col_max']
 
