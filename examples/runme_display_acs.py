6afe10f0c7cd916478004aeecb61a4d4a27c319b
Craig Jones <crjones@stsci.edu>
2018-03-07 08:39:48 -0500

    added in reading in of radec

---
+++ examples/runme_display_acs.py
@@ -0,0 +1,22 @@
+from similarity import tSNE, Jaccard, Distance
+from transfer_learning import TransferLearning
+from transfer_learning_display import TransferLearningDisplay
+
+# Load the Pickle file of data results
+filename = 'acs_19152.pck'
+tl = TransferLearning.load(filename)
+
+# Load the dictionary of RA/DEC information
+radec = pickle.load(open('/Users/crjones/christmas/hubble/ACSimages/data/kadec.pck', 'rb'))
+
+# Calculate hte similarity
+#similarity = Jaccard()
+similarity = tSNE(display_type='hexbin')
+#similarity = tSNE()
+#similarity = Distance(metric='cityblock')
+
+# Create the display with the calculated similarity
+tld = TransferLearningDisplay(similarity)
+
+# Display the figure
+tld.show(tl.fingerprints, radec=radec)
