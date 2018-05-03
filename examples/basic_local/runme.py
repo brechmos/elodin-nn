baa97cf09fc3046d1dd1a6e837bf45e76a59c4f1
Craig Jones <craig@brechmos.org>
2018-05-02 21:16:22 -0400

    pep8 and cleanup

--- examples/basic_local/runme.py
+++ examples/basic_local/runme.py
@@ -1,17 +1,15 @@
 import uuid
-import shutil
-from collections import OrderedDict
 import pickle
-import time
 
+from tldist.fingerprint.processing import FingerprintResnet
 from tldist.fingerprint.processing import calculate as fingerprint_calculate
 from tldist.similarity.processing import calculate as similarity_calculate
-import os.path
+
 
 def stringify(dictionary):
     return {k: str(v) for k, v in dictionary.items()}
 
-from tldist.fingerprint.processing import FingerprintResnet
+
 fresnet = FingerprintResnet()
 fc_save = fresnet.save()
 
@@ -22,17 +20,17 @@ processing_dict = pickle.load(open('../data/hubble_acs.pck', 'rb'))
 data = []
 for fileinfo in processing_dict[:100]:
     im = {
-        'uuid': str(uuid.uuid4()),
-        'location': fileinfo['location'],
-        'radec': fileinfo['radec'],
-        'meta': stringify(fileinfo['meta'])
-        }
-    #data_client.save(im)
+             'uuid': str(uuid.uuid4()),
+             'location': fileinfo['location'],
+             'radec': fileinfo['radec'],
+             'meta': stringify(fileinfo['meta'])
+         }
+    # data_client.save(im)
     data.append(im)
 
 fingerprints = fingerprint_calculate(data, fc_save)
 print([x['uuid'] for x in fingerprints])
 
 similarity_tsne = similarity_calculate(fingerprints, 'tsne')
- 
+
 similarity_jaccard = similarity_calculate(fingerprints, 'tsne')
