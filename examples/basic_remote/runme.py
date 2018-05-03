3aaf215d6b27e5f17d673763f7e3f6cfd45a2489
Craig Jones <craig@brechmos.org>
2018-05-02 21:18:07 -0400

    pep8 clean up

--- examples/basic_remote/runme.py
+++ examples/basic_remote/runme.py
@@ -1,23 +1,21 @@
 import uuid
-import shutil
-from collections import OrderedDict
 import pickle
-import time
 
-from celery import group
+from tldist.fingerprint.processing import FingerprintResnet
 from tldist.fingerprint.task import calculate_celery
 from tldist.similarity.task import similarity_celery
-import os.path
+
 
 def stringify(dictionary):
     return {k: str(v) for k, v in dictionary.items()}
+
 
 def chunks(l, n):
     """Yield successive n-sized chunks from l."""
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
-from tldist.fingerprint.processing import FingerprintResnet
+
 fresnet = FingerprintResnet()
 fc_save = fresnet.save()
 
@@ -31,14 +29,14 @@ for fileinfo in processing_dict[:36]:
         'location': fileinfo['location'],
         'radec': fileinfo['radec'],
         'meta': stringify(fileinfo['meta'])
-        }
-    #data_client.save(im)
+    }
+    # data_client.save(im)
     data.append(im)
 
 fingerprints = calculate_celery(data, fc_save)
 
-#fingerprints = fingerprint_client.get() 
-#print('fingerprint pks {}'.format([str(x[db.key]) for x in fingerprints]))
+# fingerprints = fingerprint_client.get()
+# print('fingerprint pks {}'.format([str(x[db.key]) for x in fingerprints]))
 
 similarity_tsne = similarity_celery(fingerprints, 'tsne')
-similarity_jaccard = similarity_celery(fingerprints, 'tsne')
+similarity_jaccard = similarity_celery(fingerprints, 'jaccard')
