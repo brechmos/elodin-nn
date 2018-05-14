c02bc0623ad48534cb9f794b073c1ba3df8d4c1a
brechmos <craig@brechmos.org>
2018-05-14 15:36:21 -0400

    added a display program

---
+++ examples/scripts/basic_local_save/runme_display.py
@@ -0,0 +1,25 @@
+from tldist.cutout import Cutout
+from tldist.database import get_database
+from tldist.similarity.display import SimilarityDisplay
+
+from configparser import ConfigParser
+config = ConfigParser()
+config.read('config.ini')
+
+# Set the database
+print('Going to setup the database in {}'.format(config['database']['filename']))
+db = get_database(config['database']['type'], config['database']['filename'])
+
+# Load the similarities
+print('Loading up the similarity data from the database')
+similarities = db.find('similarity')
+
+# Currently the similarities is a list of dictionaries where each dictionary
+# represents a similarity.  So, best is to load into a Similarity
+# instance.
+print(similarities)
+
+# Grab the first on so we can work with it.
+similarity_tsne = similarities[0]
+
+sd = SimilarityDisplay(similarity_tsne, db)
