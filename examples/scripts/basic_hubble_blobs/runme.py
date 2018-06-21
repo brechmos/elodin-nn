86677a1c9b58f273e6a74cd278b96d09d6ac4320
brechmos <craig@brechmos.org>
2018-06-21 08:44:15 -0400

    cleaned up the runme file

--- examples/scripts/basic_hubble_blobs/runme.py
+++ examples/scripts/basic_hubble_blobs/runme.py
@@ -1,6 +1,4 @@
 import pickle
-import os
-import shutil
 import numpy as np
 
 from transfer_learning.fingerprint.processing import FingerprintCalculatorResnet
@@ -14,17 +12,25 @@ from configparser import ConfigParser
 config = ConfigParser()
 config.read('config.ini')
 
-# Create the database
-print('Going to setup the database in {}'.format(config['database']['filename']))
+#
+# Load the pickle file with the location and meta information.
+#
 
-# Load the data
 print('Loading the Hubble meta data and location information')
 processing_dict = pickle.load(open('../../data/hubble_acs.pck', 'rb'))
 
+#
+# Create data pre-processing steps.
+#
+
+gray_scale = image_processing.GrayScale()
 data_crop = image_processing.Crop([15, -15, 15, -15])
 
+#
+# Load up the data
+#
+
 print('Setting up the data structure required')
-gray_scale = image_processing.GrayScale()
 data_collection = DataCollection()
 np.random.seed(12)
 for fileinfo in np.random.choice(processing_dict, 800, replace=False):
@@ -36,31 +42,40 @@ for fileinfo in np.random.choice(processing_dict, 800, replace=False):
     data_collection.add(im)
 
 #
+# Cutout generator
+#
+
+blob_cutout = BlobCutoutGenerator(output_size=(224, 224), mean_threshold=2.0,
+                                  gaussian_smoothing_sigma=2, label_padding=10)
+
+#
 #  Create cutouts
 #
 
-# Cutout processing
-cutout_resize = image_processing.Resize([224, 224])
-#cutout_histeq = image_processing.HistogramEqualization()
-
-# Cutout generator
-blob_cutout = BlobCutoutGenerator(output_size=(224, 224), mean_threshold=2.0, gaussian_smoothing_sigma=2, label_padding=10)
-
 print('Going to create the cutouts')
+
+cutout_resize = image_processing.Resize([224, 224])
 cutout_processing = [cutout_resize]
 cutouts = blob_cutout.create_cutouts(data_collection,
                                      cutout_processing=cutout_processing)
 
+#
 # Create the fingerprint calculator... fingerprint
+#
+
 print('Creating the info for the fingerprint calculator')
 fresnet = FingerprintCalculatorResnet()
 fc_save = fresnet.save()
+
+#
+#  Calculate the fingerprints from the cutouts.
+#
 
 print('Calculating the fingerprints')
 fingerprints = fingerprint_calculate(cutouts, fc_save)
 
 #
-# # An example method of filtering fingerprints
+# Calculate the fingerprint similarity
 #
 
 print('Calculating the tSNE similarity')
