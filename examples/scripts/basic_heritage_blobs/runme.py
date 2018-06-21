b488dd6451da28722df0d9860a0099f017541721
brechmos <craig@brechmos.org>
2018-06-21 08:40:25 -0400

    cleaned up runme file

--- examples/scripts/basic_heritage_blobs/runme.py
+++ examples/scripts/basic_heritage_blobs/runme.py
@@ -7,10 +7,7 @@ from transfer_learning.fingerprint.processing import calculate as fingerprint_ca
 from transfer_learning.similarity.similarity import calculate as similarity_calculate
 from transfer_learning.misc.image_processing import Resize
 from transfer_learning.data import Data, DataCollection
-from transfer_learning.cutout import CutoutCollection
 from transfer_learning.cutout.generators import BlobCutoutGenerator
-
-fc_save = FingerprintCalculatorResnet().save()
 
 config = ConfigParser()
 config.read('config.ini')
@@ -20,13 +17,11 @@ config.read('config.ini')
 #
 
 print('Going to calculate the sliding window cutouts')
-blob_cutout = BlobCutoutGenerator(output_size=(224, 224), mean_threshold=2.0, gaussian_smoothing_sigma=2, label_padding=10)
-
-resize_224 = Resize(output_size=(224, 224))
+blob_cutout = BlobCutoutGenerator(output_size=(224, 224), mean_threshold=2.0,
+                                  gaussian_smoothing_sigma=2, label_padding=10)
 
 print('Going to load the HST Heritage data')
 data = DataCollection()
-cutouts = CutoutCollection()
 for filename in glob.glob('../../data/heritage/*.???'):
 
     #
@@ -35,24 +30,27 @@ for filename in glob.glob('../../data/heritage/*.???'):
 
     print('   processing {}'.format(filename))
     image_data = Data(location=filename, radec=(-32, 12), meta={})
-    image_data.get_data()
     data.add(image_data)
 
-    #
-    #  Create the cutouts
-    #
-    imcutouts = blob_cutout.create_cutouts(image_data, cutout_processing=[resize_224])
-    cutouts = cutouts + imcutouts
+#
+#  Create the cutouts
+#
+
+resize_224 = Resize(output_size=(224, 224))
+cutouts = blob_cutout.create_cutouts(data, cutout_processing=[resize_224])
 
 #
 #  Compute the fingerprints for each cutout
 #
+
 print('Calculate the fingerprint for each cutout')
+fc_save = FingerprintCalculatorResnet().save()
 fingerprints = fingerprint_calculate(cutouts, fc_save)
 
 #
 #  Compute the similarity metrics
 #
+
 print('Calculating the tSNE similarity')
 similarity_tsne = similarity_calculate(fingerprints, 'tsne')
 with open('similarity_tsne.pck', 'wb') as fp:
